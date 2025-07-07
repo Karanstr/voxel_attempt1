e3d98fbe59c72aa2f8e19505040f355b5f5ab9b9
Karanstr <139508165+Karanstr@users.noreply.github.com>
2025-07-07 04:06:44 -0400

    Replaced shell.nix with devenv

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+let 
+  dyn_libs = with pkgs; [
+    libxkbcommon
+    wayland
+    vulkan-loader
+  ];
+in {
+  packages = dyn_libs ++ [ 
+    pkgs.rust-analyzer
+    pkgs.linuxPackages_latest.perf 
+  ];
+
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    # We need to add the language server manually so we can add it to the path correctly
+    components = [ "rustc" "cargo" "clippy" "rustfmt" ];
+    rustflags = "-C link-arg=-Wl,-rpath,${pkgs.lib.makeLibraryPath dyn_libs} ";
+  };
+
+  # Throws path into the env for nvim lsp
+  env.RUST_ANALYZER = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+}
